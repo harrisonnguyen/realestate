ad114812971695d67c09cc32fd66a4496132ada1
harrison <harrison.nguyen12@gmail.com>
2015-12-04 10:15:05 +1100

    working scrape with single page

--- realestate/realestate/spiders/real_estate_spider.py
+++ realestate/realestate/spiders/real_estate_spider.py
@@ -1,6 +1,6 @@
 import scrapy
 
-# from realestate.items import RealEstateItem
+from realestate.items import RealEstateItem
 
 # class RealEstateSpider(scrapy.Spider):
 	# name = "realestate"
@@ -30,7 +30,7 @@ class RealEstateSpider(scrapy.Spider):
 	def parse(self, response):
 		item = RealEstateItem()
 		item['address'] = response.xpath('//div[4]/div/div[1]/div[3]/h3/text()').extract()
-		item['postalAddress']= response.xpath('//div[4]/div/div[1]/div[1]/div[1]/h1/span[4]/text()').extract()
+		item['postalCode']= response.xpath('//div[4]/div/div[1]/div[1]/div[1]/h1/span[4]/text()').extract()
 		item['propertyType'] = response.xpath('//div[4]/div/div[1]/div[1]/div[3]/ul/li[2]/span/text()').extract()
 		item['price'] = response.xpath('//div[4]/div/div[1]/div[1]/div[3]/ul/li[1]/p/text()').extract()
 		item['bedrooms'] = response.xpath('//div[4]/div/div[1]/div[6]/div[1]/div/ul[1]/li[3]/span/text()').extract()
