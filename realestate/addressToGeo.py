05812cefb9ce5a25368cd217dad2bd630905ca8d
harrison@home <harrison.nguyen12@gmail.com>
2015-12-13 21:49:48 +1100

    change address ToGeo code

--- realestate/addressToGeo.py
+++ realestate/addressToGeo.py
@@ -5,7 +5,6 @@ import re
 import sys
 import urllib
 import csv
-import shapefile
 
 
 def main():
@@ -14,55 +13,69 @@ def main():
 def getLatLong(coord, address):
     
 	#formats the string to make it a valid url
-	address = urllib.quote(address, safe='') 
+	#address = urllib.quote(address, safe='') 
 	#forms the http request
-	httpRequest = "https://maps.googleapis.com/maps/api/geocode/json?address="  + address 
-	
-
+	httpRequest = "https://maps.googleapis.com/maps/api/geocode/xml?address=" + address +"%NSW&key=AIzaSyC3S-4_i8u4oPzm7WZFtG-xX8UMxLRb00k"
+	#httpRequest = "https://maps.googleapis.com/maps/api/geocode/xml?address=128%20Botany%20Road%20Alexandria%NSW&key=AIzaSyC3S-4_i8u4oPzm7WZFtG-xX8UMxLRb00k"
+	#print httpRequest
     #gets the page
 
 	page = requests.get(httpRequest) 
 	#content = page.text
-	content = page.content.split('\n')
+	content = page.content.split()
 	latitude = 0
 	longitude = 0
-
-	for line in content:
-		#find matches for latitude and longitude
-
-		if re.search("lat", line):
-			line = re.sub('.*: ', '', line)
-			line = re.sub(',.*', '', line)
-			latitude = line
-		if re.search("lng", line):
-			line = re.sub('.*: ', '', line)
-			line = re.sub(',.*', '', line)
-			longitude = line
-		#longitude is after latitude in the file
+	content.reverse()
+	#print content
+	while True:
+		string = content.pop()
+		#print string
+	#find matches for latitude and longitude
+		if "<status>OVER" in string:
+			coord.append(float(999.999))
 			break
-
+		if "<location>" in string:
+			string = content.pop()
+			string = re.sub("<lat>",'',string)
+			string = re.sub("</lat>",'',string)
+			latitude = string.strip(' ').strip('\n')
+			string = content.pop()
+			string = re.sub("<lng>",'',string)
+			string = re.sub("</lng>",'',string)
+			longitude = string.strip(' ').strip('\n')
+			break
+		if "</result>" in string:
+			#reached end of page
+			coord.append(float(999.999))
+			break;
+	#print latitude, longitude
 	coord.append(float(longitude))
 	coord.append(float(latitude))
 
     
     
 def makeShapeFile():
-	with open("output.csv",'r') as p:
-		with open("realestatedata.csv",'w') as csvoutput:
-			fieldnames = ['bathrooms', 'car', 'price','bedrooms', 'descript', 'address', 'postalCode', 'propertyType', 'coordinates']
+	with open("realestatedata2000_2040.csv",'r') as p:
+		with open("realestatedata2000_2040a.csv",'w') as csvoutput:
+			fieldnames = ['sold_date','bathrooms', 'car', 'price','bedrooms', 'descript', 'address', 'postalCode', 'propertyType', 'coordinates']
 			writer = csv.DictWriter(csvoutput, fieldnames=fieldnames, lineterminator='\n')
 			reader = csv.DictReader(p)
 			writer.writeheader()
 			#i = 0
 			for row in reader:                    
-				address = row["address"]          
-				coord = []            
-				getLatLong(coord, address)
-				row.update({'coordinates' : str(coord[0]) + "," +  str(coord[1])})
+				if "0.0" in row['coordinates'] or not row['coordinates'] or "999.999" in row['coordinates']:
+					address = row["address"]          
+					coord = []
+					getLatLong(coord, address)
+					if coord[0] == 999.999:
+						print "Status over query limit"
+						break
+					else:
+						row.update({'coordinates' : str(coord[0]) + "," +  str(coord[1])})
 				#if coord[0] == 0: #if the api calls are exhausted
 				#	print row
 				#	break                        
-				#print i, ": ", str(coord[0]) + "," +  str(coord[1])
+						print address, ": ", str(coord[0]) + "," +  str(coord[1])
 				#i += 1
 				writer.writerow(row)
 main()
